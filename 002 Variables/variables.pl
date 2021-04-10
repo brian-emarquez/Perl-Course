6cf8d153d287b9a92c7f15bff6b030d1884702da
Brian Marquez <brian3marquez@gmail.com>
2021-04-09 23:53:20 -0500

    Matrices

---
+++ 002 Variables/variables.pl
@@ -0,0 +1,16 @@
+# Variables
+
+use strict; # libreria de correcta escrituraS
+use warnings; # para mostrar advertencias
+
+#escalares
+
+my $nombre = "Brian Marquez";   
+my $numero = 56; 
+
+# se concatena con .
+print $nombre . "\n";
+print $numero . "\n";
+
+exit;
+
