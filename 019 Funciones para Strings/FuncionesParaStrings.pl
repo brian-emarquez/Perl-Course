47f790fe929f963351e208656a3f398f05c58867
Brian Marquez <brian3marquez@gmail.com>
2021-04-13 18:02:33 -0500

    Funciones para Strings

--- 019 Funciones para Strings/FuncionesParaStrings.pl
+++ 019 Funciones para Strings/FuncionesParaStrings.pl
@@ -1,8 +1,57 @@
 #!c:/Strawberry/perl/bin/perl.exe
-use stri
-# funcion binmode;
 
-binmode (STDOUT, "Escoding (csp850)");
+use strict;
+use utf8;
+
+# funcion binmode;
+binmode(STDOUT, ":encoding(cp850)"); # usada para mostrar cacteres con tildes, etc.
+
+my $string = "La condición \n";
+print $string;
+
+print "________________________CHOMP____________________________\n";
+# Elimina el salto de linea del final de un string
+$string = "Hola Mundo \n";
+chomp($string);
+print $string."\n";
+
+print "________________________CHOP____________________________\n";
+# chop: Elinar el ultimo caracteres de un string
+my $string1 = "012345";
+chop($string1);
+print $string1."\n";
+
+print "________________________UC_____________________________\n";
+# uc: convierte el string a mayusculas
+my $string2 = "hola mundo";
+print uc($string2);
+print "\n";
+
+print "________________________LC_____________________________\n";
+# lc: convierte de mayusculas a minusculas
+my $string3 = "BRIAN";
+print lc($string3);
+print "\n";
+
+print "________________________LENGTH_____________________________\n";
+# length: Regresa la longitus total del string seleccionado
+my $string4 = "brian";
+print length($string4);
+print "\n";
+
+print "________________________SUBSTR_____________________________\n";
+# Regresa el subtraing a traves de las coordenas iniio, final, si es negatico la fila de extraccion se realiza de derecha a ixquierda
+my $string5 = "hola mundo";
+print substr($string5, 0, 4);
+print "\n";
+
+print "________________________INDEX_____________________________\n";
+# Regresa el subtraing a traves de las coordenas iniio, final, si es negatico la fila de extraccion se realiza de derecha a ixquierda
+my $string6 = "programando con perl";
+my $posicion = index($string6, 'perl', 0);
+print "lo posicion de perl en el string : $string6 es $posicion";
+print "\n";
+
 
 
 exit;
--- 019 Funciones para Strings/readme.md
+++ 019 Funciones para Strings/readme.md
@@ -1,6 +1,31 @@
-## Funcion para String
+### Funcion para String
 
 - `binmode` : permite indicar la codificacion corracta para la entrada y la salidad de datos.
 
 - Esta funcion permite indicar la codificacion cp850 de la consola de windows para evitar que aparescan caracteres extraños
+
+### Chomp 
+
+- Eliminar el salto de linea del dinal de un string
+
+### chop
+
+- Elimanr el ultimo catacter de un string
+
+### UC
+Convierte el string a mayusculas
+
+### LC
+
+- Convierte de mayusculas a minuscula
+ 
+### Length
+
+- length: Regresa la longitus total del string seleccionado
+
+### SUBATR
+
+- Regresa el subtraing a traves de las coordenas iniio, final, si es negatico la fila de extraccion se realiza de derecha a ixquierda
+
+### Index: devuelve la posicion de la primera ocurrencia del subtring, la posicion inicla es de 0, si el substring no es encontrado devuelve -1
 
