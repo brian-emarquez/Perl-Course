0be5c206ff3ee413eb42ed246b4f269451c17404
Brian Marquez <brian3marquez@gmail.com>
2021-04-13 19:55:54 -0500

    Funciones con Arrays UPDATE
    Funciones con arrays UPDATE
    
    see also:
    resolves:

--- 019 Funciones para Strings/FuncionesParaStrings.pl
+++ 019 Funciones para Strings/FuncionesParaStrings.pl
@@ -5,30 +5,31 @@ use utf8;
 
 # funcion binmode;
 binmode(STDOUT, ":encoding(cp850)"); # usada para mostrar cacteres con tildes, etc.
+binmode(STDIN, ":encoding(cp850)");
 
 my $string = "La condición \n";
 print $string;
 
-print "________________________CHOMP____________________________\n";
+print "_________________________CHOMP____________________________\n";
 # Elimina el salto de linea del final de un string
 $string = "Hola Mundo \n";
 chomp($string);
 print $string."\n";
 
-print "________________________CHOP____________________________\n";
+print "_________________________CHOP____________________________\n";
 # chop: Elinar el ultimo caracteres de un string
 my $string1 = "012345";
 chop($string1);
 print $string1."\n";
 
-print "________________________UC_____________________________\n";
+print "__________________________UC_____________________________\n";
 # uc: convierte el string a mayusculas
 my $string2 = "hola mundo";
 print uc($string2);
 print "\n";
 
-print "________________________LC_____________________________\n";
-# lc: convierte de mayusculas a minusculas
+print "_________________________LC_____________________________\n";
+# lc: convierte de mayuscula:s a minusculas
 my $string3 = "BRIAN";
 print lc($string3);
 print "\n";
@@ -47,11 +48,9 @@ print "\n";
 
 print "________________________INDEX_____________________________\n";
 # Regresa el subtraing a traves de las coordenas iniio, final, si es negatico la fila de extraccion se realiza de derecha a ixquierda
-my $string6 = "programando con perl";
+my $string6 = "programando con pE";
 my $posicion = index($string6, 'perl', 0);
 print "lo posicion de perl en el string : $string6 es $posicion";
 print "\n";
-
-
 
 exit;
--- 020 Funciones para Arrays/FuncionesArrays.pl
+++ 020 Funciones para Arrays/FuncionesArrays.pl
@@ -2,14 +2,62 @@
 
 #!c:/Strawberry/perl/bin/perl.exe
 
-use strict;
-use utf8;
-
 binmode(STDOUT, ":encoding(cp850)"); # usada para mostrar cacteres con tildes, etc.
 binmode(STDIN, ":encoding(cp850)");
 
-print "______________ESCALAR_________________"
+use strict;
+use utf8;
+
+print "______________ESCALAR_________________\n";
 # escalar: devuelve el numero total de elemento de un array
-my @lista = {1, 2, 3, 4, 5};
+my @lista = (1, 2, 3, 4, 5);
 print scalar(@lista);
-print ".\n";
+print "\n";
+
+print "______________JOIN_________________\n";
+# JOIN: cONVIERTE UN ARRAY DE UN STRING MEDIANTE EL SEPARADOR INDICADO
+my @numero = ("uno", "dos", "tres", "cuatro", "cinco");
+my $string = join ('-', @numero);
+print $string;
+print "\n";
+
+print "______________SPLIT_________________\n";
+# split: divide un strin para convertirlo en un array a partir del patron indicado
+my $cadena = "uno dos trer cuatro";
+my @lista = split(' ', $cadena);
+foreach(@lista){
+    print $_."\n";
+
+print "______________SHIFT_________________\n";
+# SHIFT: Elimina el primer elemento de un array
+my @nombres = ('Raul', 'Antonio', 'Padre');
+shift(@nombres);
+foreach (@nombres){
+    print $_."\n";
+}
+
+print "______________UNSHIFT_________________\n";
+# UNSHIFT: Agregar un ellemento en el array
+unshift(@nombres, 'brian');
+foreach (@nombres){
+    print $_."\n";
+}
+
+print "______________POP_________________\n";
+# POP: Elimina el ultimo elemento del array
+my @paises = ('Peru', 'Chile', 'Ecuador', 'Argentina');
+pop(@paises);
+foreach (@paises){
+    print $_."\n";
+}
+
+print "______________PUSH_________________\n";
+# Push: AGREGAR UNO O MAS ELEMENTOS AL FINAL
+my @frutas = ('manzana', 'pera', 'piña', 'sandia');
+push(@frutas, 'toronja');
+foreach (@frutas){
+    print $_."\n";
+}
+
+
+}
