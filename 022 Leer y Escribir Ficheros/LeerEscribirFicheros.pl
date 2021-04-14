fb69563da5d332d97c98381962c080a79c0386dd
Brian Marquez <brian3marquez@gmail.com>
2021-04-13 21:15:01 -0500

    Update

--- 022 Leer y Escribir Ficheros/LeerEscribirFicheros.pl
+++ 022 Leer y Escribir Ficheros/LeerEscribirFicheros.pl
@@ -1,1 +1,29 @@
-.pl
+#!c:/Strawberry/perl/bin/perl.exe
+
+# exita caracteres extraños en las consolo de windows
+binmode(STDOUT, ":encoding(cp850)");
+binmode(STDIN, ":encoding(cp850)");
+
+use strict;
+use utf8;
+
+# Metodos
+#< read
+#> white 
+#>>append
+
+open (ARCHIVO, '<', 'text.txt');
+
+print "Agrega contenido al archivo texto: ";
+my $contenido = <STDIN>;
+
+print ARCHIVO $contenido;
+close (ARCHIVO);
+
+print "Mostar el contenido\n";
+open(ARCHIVO, '<', 'text.txt') ;
+while(<ARCHIVO>){
+    print $_;
+}
+
+exit;
